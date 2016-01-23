ed4b6eb835109c94c98ae3e4816f1cff205e0960
m1ke2014 <m1ke02@yahoo.co.uk>
2016-01-23 08:16:21 +0000

    Initial

---
+++ FinalYearProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FinalYearProject.Startup))]
+namespace FinalYearProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
