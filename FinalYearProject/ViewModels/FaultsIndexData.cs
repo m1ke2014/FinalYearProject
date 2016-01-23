cbfc54d30401bc0e4c6b58fe7c7a172d76974fc9
m1ke2014 <m1ke02@yahoo.co.uk>
2016-01-23 08:34:41 +0000

    Added ViewModels

---
+++ FinalYearProject/ViewModels/FaultsIndexData.cs
@@ -0,0 +1,14 @@
+﻿using FinalYearProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FinalYearProject.ViewModels
+{
+    public class FaultsIndexData
+    {
+        public IEnumerable<Product> Products { get; set; }
+        public IEnumerable<Fault> Faults { get; set; }
+    }
+}
---
+++ FinalYearProject/ViewModels/SelectedChemicalsData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FinalYearProject.ViewModels
+{
+    public class SelectedChemicalsData
+    {
+        public int ChemicalID { get; set; }
+        public string Description { get; set; }
+        public bool Assigned { get; set; }
+    }
+}
